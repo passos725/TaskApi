26d6d51b5194b66fa5c2b2e4f507de801106eb27
passos725 <passos725@gmail.com>
2022-08-11 19:39:29 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+
+const routes = new Router();
+routes.post('/Users', UserController.store);
+export default routes;
