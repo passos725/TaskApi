b9c985aa16b8b240b44064e2dc9058b753c8c7bf
passos725 <passos725@gmail.com>
2022-08-12 20:07:56 -0300

    Validation added
    validation using yup schema

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
+  // Store the info
+
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(8).max(15),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'falha na validação' });
+    }
+
     const userExist = await User.findOne({
       where: { email: req.body.email },
     });
@@ -13,7 +26,27 @@ class UserController {
     return res.json({ id, name, email });
   }
 
+  // Update the info
+
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(8).max(15),
+      password: Yup.string()
+        .min(8)
+        .max(15)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'falha na validação' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
