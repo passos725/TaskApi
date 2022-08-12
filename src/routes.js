7501c1a78c74cee92b351a17b629a969d2340742
passos725 <passos725@gmail.com>
2022-08-12 19:37:14 -0300

    new changes
    Addition of the method of updating the data in the database with verification of the token and if the information matches, if so, it will be updated in the database, if not, an error will be returned

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import sessionController from './app/controllers/sessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-routes.post('/Users', UserController.store);
+routes.post('/users', UserController.store);
 routes.post('/sessions', sessionController.store);
-routes.put('/Users', UserController.update);
+
+// Routes bellow this one needs auth token
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 export default routes;
