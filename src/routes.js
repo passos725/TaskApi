4073607ac75ec931d0a20cfa3d140fa2958c4f52
passos725 <passos725@gmail.com>
2022-08-12 22:46:31 -0300

    New Functions
    Addeds a task create, check when is finished and deleted

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import sessionController from './app/controllers/sessionController';
-import authMiddleware from './app/middlewares/auth';
+import TaskController from './app/controllers/TaskController';
 
 const routes = new Router();
 routes.post('/users', UserController.store);
@@ -9,5 +12,10 @@ routes.post('/sessions', sessionController.store);
 
 // Routes bellow this one needs auth token
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+routes.post('/tasks', TaskController.store);
+routes.get('/tasks', TaskController.index);
+routes.put('/tasks/:task_id', TaskController.update);
+routes.delete('/tasks/:task_id', TaskController.delete);
 export default routes;
